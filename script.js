efc6d9668b7d7978a2aecb1ffadbd543221bb1d8
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-01 11:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-//your JS code here. If required.
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+
+let currActive = 1;
+
+nextBtn.addEventListener("click", () => {
+  currActive++;
+  if (currActive > circles.length) {
+    currActive = circles.length;
+  }
+  update();
+});
+
+prevBtn.addEventListener("click", () => {
+  currActive--;
+  if (currActive < 1) {
+    currActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".circle.active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  prevBtn.disabled = currActive === 1;
+  nextBtn.disabled = currActive === circles.length;
+}
+
+// initialize state
+update();
